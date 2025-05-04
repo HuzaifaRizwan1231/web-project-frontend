6165adc46bbb8f7ceac5561e4f335cb863b1ced3
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-05-04 20:05:56 +0500

    fix: implement getUserApiCall in ProtectedRoute and remove hardcoded user data

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -6,7 +6,7 @@ export const signUpApiCall = async (body) => {
   try {
     const response = await api.post(`${path}/signup`, body);
     return response;
-     } catch (error) {
+  } catch (error) {
     return error.response.data;
   }
 };
@@ -14,6 +14,36 @@ export const signUpApiCall = async (body) => {
 export const loginApiCall = async (body) => {
   try {
     const response = await api.post(`${path}/login`, body);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const getUserApiCall = async () => {
+  try {
+    // const response = await api.get(`${path}/user`);
+    const response = await {
+      data: {
+        success: true,
+        data: {
+          user: {
+            username: "Khizr",
+            email: "khiz6@gmail.com",
+            password:
+              "$2b$10$w1IFVpwXL11SLr6cvOuOnu9Yl3fcii1lyildzz7Pb.ADAB0zbnO4S",
+            profileImage: "https://www.w3schools.com/howto/img_avatar.png",
+            createdAt: {
+              $date: "2025-04-24T13:48:32.761Z",
+            },
+            updatedAt: {
+              $date: "2025-04-24T13:48:32.761Z",
+            },
+            __v: 0,
+          },
+        },
+      },
+    };
     return response.data;
   } catch (error) {
     return error.response.data;
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import { setUser } from "../redux/features/user/userSlice";
+import { getUserApiCall } from "../api/auth.api";
 
 const ProtectedRoute = () => {
   // states
@@ -12,25 +13,7 @@ const ProtectedRoute = () => {
   const dispatch = useDispatch();
 
   const getUser = async () => {
-    const response = await {
-      success: true,
-      data: {
-        user: {
-          username: "b1",
-          email: "khiz6@gmail.com",
-          password:
-            "$2b$10$w1IFVpwXL11SLr6cvOuOnu9Yl3fcii1lyildzz7Pb.ADAB0zbnO4S",
-          profileImage: "https://www.w3schools.com/howto/img_avatar.png",
-          createdAt: {
-            $date: "2025-04-24T13:48:32.761Z",
-          },
-          updatedAt: {
-            $date: "2025-04-24T13:48:32.761Z",
-          },
-          __v: 0,
-        },
-      },
-    };
+    const response = await getUserApiCall();
 
     if (response.success) {
       dispatch(setUser(response.data.user));
